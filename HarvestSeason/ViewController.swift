ce626c7e6c9148be053acc9b59cd451db14ff106
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-12-19 09:55:40 +1100

    Xcode.init

---
+++ HarvestSeason/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HarvestSeason
+//
+//  Created by Nicholas Tian on 19/12/2016.
+//  Copyright © 2016 nicktd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
